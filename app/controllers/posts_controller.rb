cf3d92723ce1572e72303dea9da23cd38b602607
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-12 17:49:47 +0200

    Add body column to post and add boostrap gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def new
+    @post = Post.new
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_151426) do
+ActiveRecord::Schema.define(version: 2020_11_12_153953) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "body"
   end
 
   create_table "users", force: :cascade do |t|
