d9cff7ed7aa9e54cf679ceaacbd3e4ee6029ef87
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-11-12 12:58:26 -0400

    Add index, new, views and post and errors  partials for post resource. Add index view for the root route. Update routes to make the right connections. Update the create and index methods in the post controller."

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,24 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      flash[:notice] = "Post was created successfully."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
